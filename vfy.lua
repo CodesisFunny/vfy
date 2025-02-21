8b8df293e2a1e883d7ac4c39f34a96270242ff52
CodesisFunny <119093926+CodesisFunny@users.noreply.github.com>
2025-02-21 09:39:58 +0700

    Create vfy.lua

---
+++ vfy.lua
@@ -0,0 +1,15 @@
+{
+    "whitelist": {
+        "pelebey1": {
+            "userid": "576643399",
+            "username": "pelebey1",
+            "displayname": "June",
+            "accountage": "2496"
+        },
+        "itthe34": {
+            "userid": "2211523758",
+            "username": "itthe34",
+            "displayname": "Ato",
+            "accountage": "1525"
+        },
+}
